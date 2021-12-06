da1d3f1f7e9ca2c5ec839a22bb0e5ab9e6cb0393
Amber <martinez.amber1028@gmail.com>
2021-12-06 12:25:43 -0500

    commit tree 39748ad112eeaa69ccc103f0a8e16a5f7b8879b4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Amber", "Stephen", "Nicole"];
+
+function writeCards(names, event) {
+let newArr = []
+  for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+  }
+  return newArr
+}
+
+writeCards(names, "anniversary");
+
+function countDown(number) {
+    while (number > -1) {
+      console.log(number)
+      number--
+    }
+}
